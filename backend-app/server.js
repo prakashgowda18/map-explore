1135d9deb65b7cb58feea53090fcdf4b1f6bdf77
Vimukthi Kulathilaka <94285196+VimukthiMK@users.noreply.github.com>
2024-02-08 17:38:15 +0530

    Backend-app: installed dotenv

--- backend-app/server.js
+++ backend-app/server.js
@@ -3,9 +3,7 @@ const mongoose = require("mongoose")
 const app = express()
 const port = 8800
 
-app.get('/',(req,res)=>{
-    res.send("Hello")
-})
+
 
 app.listen(port,()=>{
     console.log("Backend is running")
