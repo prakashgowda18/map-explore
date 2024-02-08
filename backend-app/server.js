6c94524c5ebd14e4dbeb5d857873155822cabbdc
Vimukthi Kulathilaka <94285196+VimukthiMK@users.noreply.github.com>
2024-02-08 18:47:30 +0530

    Backend-app : Connected MongoDB database

--- backend-app/server.js
+++ backend-app/server.js
@@ -1,9 +1,19 @@
 const express = require("express")
 const mongoose = require("mongoose")
+const dotenv = require("dotenv")
+
+//initialize the app
 const app = express()
+//initialize the port
 const port = 8800
+// dotenv config
+dotenv.config()
 
-
+/*db connection
+MONGO_URL = 'mongodb+srv://user:1234@cluster0.lbbedwq.mongodb.net/?retryWrites=true&w=majority'*/
+mongoose.connect(process.env.MONGO_URL)
+.then(() => console.log("MongoDB connected!"))
+.catch(err => console.log(err));
 
 app.listen(port,()=>{
     console.log("Backend is running")
