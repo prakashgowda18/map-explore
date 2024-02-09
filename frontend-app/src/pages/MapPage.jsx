fcbcfc7009c14a4fcb0a7dcb7206ca6a708a3a96
Vimukthi Kulathilaka <94285196+VimukthiMK@users.noreply.github.com>
2024-02-09 15:30:34 +0530

    Frontend-app: added navigation control to map and map css

--- frontend-app/src/pages/MapPage.jsx
+++ frontend-app/src/pages/MapPage.jsx
@@ -1,19 +1,25 @@
 import React from 'react'
-import Map  from 'react-map-gl'
+import Map, { Marker,NavigationControl } from 'react-map-gl'
+import 'mapbox-gl/dist/mapbox-gl.css';
 
-const MapPage=()=>{
-  return (
-    <div style={{ height: "100vh", width: "100%" }}>
-      <Map
-      mapboxAccessToken={process.env.REACT_APP_MAPBOX}  
-      initialViewState={{
-        longitude: 79.861244,
-        latitude: 	6.927079,
-        zoom: 3
-      }}
-      mapStyle="mapbox://styles/mapbox/streets-v9"
-    />
-    </div>
-  )
+const MapPage = () => {
+    return (
+        <div style={{ height: "100vh", width: "100%" }}>
+            <Map
+                mapboxAccessToken={process.env.REACT_APP_MAPBOX}
+                initialViewState={{
+                    longitude: 79.861244,
+                    latitude: 6.927079,
+                    zoom: 4
+                }}
+                mapStyle="mapbox://styles/mapbox/streets-v9"
+            >
+                <Marker longitude={79.861244} latitude={6.927079} anchor="bottom" >
+                </Marker>
+                <NavigationControl position='bottom-right'/>
+
+            </Map>
+        </div>
+    )
 }
 export default MapPage
