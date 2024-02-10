e6c8ea347fcc64966238da5b91c5e71351426ccd
Vimukthi Kulathilaka <94285196+VimukthiMK@users.noreply.github.com>
2024-02-11 00:12:21 +0530

    Frontend-app: Implemented mapbox Popup for location

--- frontend-app/src/pages/MapPage.jsx
+++ frontend-app/src/pages/MapPage.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
-import Map, { Marker,NavigationControl } from 'react-map-gl'
+import React, { useState } from 'react'
+import Map, { Marker, NavigationControl, Popup } from 'react-map-gl'
 import 'mapbox-gl/dist/mapbox-gl.css'
-import LocationOnIcon from '@mui/icons-material/LocationOn';
+import LocationOnIcon from '@mui/icons-material/LocationOn'
+import StarIcon from '@mui/icons-material/Star'
+import './mappage.css'
+
 
 const MapPage = () => {
+    const [showPopup, setShowPopup] = useState(true)
     return (
         <div style={{ height: "100vh", width: "100%" }}>
             <Map
@@ -15,10 +19,37 @@ const MapPage = () => {
                 }}
                 mapStyle="mapbox://styles/mapbox/streets-v9"
             >
+                <NavigationControl position='top-right' />
+
                 <Marker longitude={79.861244} latitude={6.927079} anchor="bottom" >
-                    <LocationOnIcon style={{fontSize:visualViewport.zoom*10, color:'slateblue'}}/>
+                    <LocationOnIcon style={{ fontSize: visualViewport.zoom * 10, color: 'slateblue' }} />
                 </Marker>
-                <NavigationControl position='top-right'/>
+
+                {showPopup && (
+                    <Popup longitude={79.861244} latitude={6.927079}
+                        anchor="left"
+                        onClose={() => setShowPopup(false)}>
+                        <div className="card">
+                            <label>Place</label>
+                            <h4 className="place">Colombo</h4>
+                            <label>Review</label>
+                            <p className="desc">Noice</p>
+                            <label>Rating</label>
+                            <div className="stars">
+                            <StarIcon />
+                            <StarIcon />
+                            <StarIcon />
+                            <StarIcon />
+                            <StarIcon />
+                            </div>
+                            <label>Information</label>
+                            <span className="username">
+                                Created by <b> Amal</b>
+                            </span>
+                            <span className="date"> 2hrs ago</span>
+                        </div>
+                    </Popup>)}
+
 
             </Map>
         </div>
