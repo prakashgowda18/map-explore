27fd0aacbcd2fd9a0b155cae94ffc438a34e4f15
Vimukthi Kulathilaka <94285196+VimukthiMK@users.noreply.github.com>
2024-02-12 23:17:42 +0530

    Backend-app : Restructure project files

--- backend-app/routes/pins.js
+++ backend-app/routes/pins.js
@@ -1,29 +1,9 @@
-const router = require("express").Router()
-const Pin = require("../models/pin") // Pin model
+const express = require('express')
+const router = express.Router()
+const pinController = require('../controllers/pinController')
 
-// Create a pin
-router.post("/", async (req, res) => {
-  try {
-    const newPin = new Pin(req.body)
-    const savedPin = await newPin.save()
-    res.status(201).json(savedPin) // Status 201 - resource created
-  } catch (err) {
-    if (err.name === 'ValidationError') {
-      res.status(400).json({ message: err.message }) // Status 400 - validation error
-    } else {
-      res.status(500).json({ message: "Internal Server Error" }) // Status 500 - other errors
-    }
-  }
-})
-
-// Get all pins
-router.get("/", async (req, res) => {
-  try {
-    const pins = await Pin.find()
-    res.status(200).json(pins) // Status 200 - success
-  } catch (err) {
-    res.status(500).json({ message: "Internal Server Error" }) // Status 500 - errors
-  }
-})
+// routes
+router.post('/', pinController.createPin)
+router.get('/', pinController.getAllPins)
 
 module.exports = router
